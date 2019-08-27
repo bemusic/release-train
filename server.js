20c9535aeb8645ef1ec3ffa71aa3fb959a9fb35d
Glitch (hello-express) <dtinth@spacet.me>
2019-08-27 15:00:30 +0000

    🍜👫 Checkpoint ./views/index.html:577339/35 ./server.js:577339/1509 ./package.json:577339/2637

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // server.js
 // where your node app starts
 
+if (process.env.GIT_EMAIL) {
+  require('child_process').execSync(`git config user.email ${process.env.GIT_EMAIL}`)
+}
+
 // init project
 const express = require('express');
 const app = express();
@@ -20,3 +24,28 @@ app.get('/', function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+async function getGitHubClient() {
+  const { App } = require("@octokit/app")
+  const { request } = require("@octokit/request")
+
+  const APP_ID = +process.env.GH_APP_ID
+  const APP_INSTALLATION_ID = +process.env.GH_APP_ID
+  const PRIVATE_KEY = Bprocess.env.GH_APP_PRIVATE_KEY
+
+  const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
+  const jwt = app.getSignedJsonWebToken()
+
+  // Example of using authenticated app to GET an individual installation
+  // https://developer.github.com/v3/apps/#find-repository-installation
+  const { data } = await request("GET /repos/:owner/:repo/installation", {
+    owner: "hiimbex",
+    repo: "testing-things",
+    headers: {
+      authorization: `Bearer ${jwt}`,
+      accept: "application/vnd.github.machine-man-preview+json"
+    }
+  });
+
+  const installationId = data.id;
+}
