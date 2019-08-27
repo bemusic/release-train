cda34ea345e8c8bcaf1132104c7bd45ead0690fa
Glitch (hello-express) <dtinth@spacet.me>
2019-08-27 15:36:20 +0000

    😅📚 Checkpoint ./server.js:577339/2975 ./package.json:577339/1417

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-// server.js
-// where your node app starts
-
 if (process.env.GIT_EMAIL) {
   require('child_process').execSync(`git config user.email ${process.env.GIT_EMAIL}`)
 }
 
-// init project
+const { App } = require('@octokit/app')
+const Octokit = require('@octokit/rest')
+
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.post('/prepare', async function(req, res, next) {
+  try {
+    const gh = getGitHubClient()
+
+    // Create a preparation branch
+    octokit.pulls.list({
+  owner,
+  repo
+})
+    res.send('OK!')
+  } catch (e) {
+    next(e)
+  }
 });
 
 // listen for requests :)
@@ -25,27 +30,14 @@ const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-async function getGitHubClient() {
-  const { App } = require("@octokit/app")
-  const { request } = require("@octokit/request")
-
-  const APP_ID = +process.env.GH_APP_ID
-  const APP_INSTALLATION_ID = +process.env.GH_APP_ID
-  const PRIVATE_KEY = Bprocess.env.GH_APP_PRIVATE_KEY
-
-  const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY })
-  const jwt = app.getSignedJsonWebToken()
-
-  // Example of using authenticated app to GET an individual installation
-  // https://developer.github.com/v3/apps/#find-repository-installation
-  const { data } = await request("GET /repos/:owner/:repo/installation", {
-    owner: "hiimbex",
-    repo: "testing-things",
-    headers: {
-      authorization: `Bearer ${jwt}`,
-      accept: "application/vnd.github.machine-man-preview+json"
-    }
-  });
-
-  const installationId = data.id;
+function getGitHubClient() {
+  const app = new App({ id: process.env.GH_APP_ID, privateKey: Buffer.from(process.env.GH_APP_PRIVATE_KEY_BASE64, 'base64').toString() })
+  const octokit = new Octokit({
+   async auth () {
+     const installationAccessToken = await app.getInstallationAccessToken({ 
+       installationId: process.env.GH_APP_INSTALLATION_ID 
+     });
+     return `token ${installationAccessToken}`;
+   }
+  })
 }
